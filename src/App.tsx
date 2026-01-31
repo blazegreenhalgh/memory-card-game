6e77eae72104c4f3a0dcf87115c71c327065fd0e
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2026-01-31 20:44:44 +0500

    add select card functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,51 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [cards, setCards] = useState([1, 2, 3, 4, 5, 6]);
+  const [deck, setDeck] = useState([1, 2, 3, 4, 5, 6]);
   const [displayedCards, setDisplayedCards] = useState(Array(4).fill(null));
+  const [selectedCards, setSelectedCards] = useState([]);
 
-  const shuffleCards = (cards) => {
+  const shuffleCards = (deck) => {
     const shuffledCards = [];
     while (shuffledCards.length !== displayedCards.length) {
-      const index = Math.floor(Math.random() * cards.length);
-      if (!shuffledCards.includes(cards[index])) {
-        shuffledCards.push(cards[index]);
+      const index = Math.floor(Math.random() * deck.length);
+      if (!shuffledCards.includes(deck[index])) {
+        shuffledCards.push(deck[index]);
       }
     }
     setDisplayedCards(shuffledCards);
   };
-
   useEffect(() => {
-    shuffleCards(cards);
+    shuffleCards(deck);
   }, []);
+
+  const selectCard = (card) => {
+    const newSelectedCards = Array.from(selectedCards);
+    newSelectedCards.push(card);
+    setSelectedCards(newSelectedCards);
+  };
+
+  const handleCardClick = (e, card, deck) => {
+    shuffleCards(deck);
+    selectCard(card);
+  };
+
   return (
     <>
       <h1>Card amount: {displayedCards.length}</h1>
       {displayedCards.map((card) => (
         <button
           className="bg-white px-4 py-2 text-black cursor-pointer"
-          onClick={() => shuffleCards(cards)}
+          onClick={(e) => handleCardClick(e, card, deck)}
         >
           {card}
         </button>
+      ))}
+      <h2>Selected Cards:</h2>
+      {selectedCards.map((card) => (
+        <div className="bg-white px-4 py-2 text-black cursor-pointer">
+          {card}
+        </div>
       ))}
     </>
   );
